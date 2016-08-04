24202ff50643da35a9dda910664bc30a5d17b25c
Joe J Collins <joejcollins@gmail.com>
2016-08-04 16:40:24 +0100

    Locations plotted on map.

--- preliminary_analysis.r
+++ preliminary_analysis.r
@@ -20,30 +20,24 @@ nrow(sites)
 # Give each of the sites a unique id (might come in handy)
 sites$Id <- seq.int(nrow(sites))
 
-# Export the sites list so WGS84 can be added to it at http://gridreferencefinder.com/batchConvert/batchConvert.php
-write.table(sites, "sites.csv", row.names = FALSE, col.names = FALSE, sep = ",")
+# Export the sites list so WGS84 can be added to it
+write.table(sites, "sites.csv", row.names = FALSE, col.names = TRUE, sep = ",")
+
+## Go to http://gridreferencefinder.com/batchConvert/batchConvert.php to do the geocoding
+
+# Import the geocoded sites
+sites <- read.csv("./sites_with_locations.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# Cluster using k-means
+km <- kmeans(cbind(sites$X, sites$Y), centers = 3)
+# Plot without a background to look at the clusters
+plot(sites$X, sites$Y, col = km$cluster, pch = 20)
+plot(sites$Lng, sites$Lat, col = km$cluster, pch = 20)
+
+# Get a map of the area and plot sites
+library(ggmap)
+map_centre <- c(lon = -2.85, lat = 52.80) # chosen by inspection
+map <- qmap(map_centre) 
+map + geom_point(aes(x=Lng, y=Lat), data=sites, col = km$cluster)
 
 
-
-
-
-
-unique(pairs)
-
-
-require(rnrfa)
-
-Longitudes <- OSGparse(samples$Grid.reference, CoordSystem = "WGS84")["lon"]
-samples <- cbind(samples, Longitudes)
-
-Latitudes <- OSGparse(samples$Grid.reference, CoordSystem = "WGS84")["lat"]
-samples <- cbind(samples, Latitudes)
-
-median(samples$lon, na.rm = FALSE)
-
-median(samples$lat, na.rm = FALSE)
-
-## http://yarkerconsulting.com/index.php/blog/15-google-maps-and-r
-MyMap <- MapBackground(lat=53.04437, lon=-2.860608, zoom=2)
-
-PlotOnStaticMap(MyMap,samples$lat,samples$lon,cex=tmp+0.8,pch=16,col='black')
